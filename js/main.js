36ed01c5131176974504017f6debe59e50b4eb15
Павел Лукьянов <pahan-01.04.10@mail.ru>
2023-01-30 15:14:11 +0500

    add mobile styles

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* document.addEventListener('DOMContentLoaded', function () {
+
+    //Выбор и подтверждение города
+    let selectCityBtn = document.querySelector('.header__city__select');
+    let selectCityContainer = document.querySelector('.header__confirm__city');
+
+    selectCityBtn.addEventListener('click', () => {
+
+    })
+
+}) */
